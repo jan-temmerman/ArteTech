4ccafa24f28d98481e378c81f63bd72969defc84
Jan Temmerman <jantemme@student.arteveldehs.be>
2019-12-05 03:43:59 +0100

    added sass

---
+++ ArteTech/src/Controller/PageController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Entity\UserStatus;
+use phpDocumentor\Reflection\Types\Integer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends AbstractController
+{
+    /**
+     * @Route("/", name="page")
+     * @param Request $request
+     * @return Response
+     */
+    public function index(Request $request)
+    {
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $users = $repository->findAll();
+
+        /*$repository = $this->getDoctrine()->getRepository(UserStatus::class);
+        $statuses = $repository->findAll();
+        $status = $statuses[0];
+
+        $user = new User();
+        $user->setStatus($status);
+
+        $form = $this->createFormBuilder($user)
+            ->add('firstName', TextType::class)
+            ->add('lastName', TextType::class)
+            ->add('save', SubmitType::class, ['label' => 'Save user'])
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($data);
+            $entityManager->flush();
+
+            return $this->redirect("/");
+        }*/
+
+        return $this->render('page/index.html.twig', [
+            'users' => $users,
+            //'form' => $form->createView(),
+        ]);
+    }
+}
